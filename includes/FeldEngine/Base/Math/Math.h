d0146babcba3112ad33aa984bd082c403aa29595
Feldrise <admin@feldrise.com>
2016-06-28 12:57:00 +0200

    Add the math function into the base modul. And we change the computer for creating this project !

---
+++ includes/FeldEngine/Base/Math/Math.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace FeldEngine
+{
+	namespace Math
+	{
+		using Vector2 = glm::vec2;
+		using IVector2 = glm::ivec2;
+		using DVector2 = glm::dvec2;
+		using UVector2 = glm::uvec2;
+
+		using Vector3 = glm::vec3;
+		using IVector3 = glm::ivec3;
+		using DVector3 = glm::dvec3;
+		using UVector3 = glm::uvec3;
+
+		using Vector4 = glm::vec4;
+		using IVector4 = glm::ivec4;
+		using DVector4 = glm::dvec4;
+		using UVector4 = glm::uvec4;
+
+		using Mat2 = glm::mat2;
+		using Mat3 = glm::mat3;
+		using Mat4 = glm::mat4;
+	}
+}
