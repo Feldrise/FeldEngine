3c6b96e5891cce6ec7ce86b65a82a13e80880b49
Feldrise <admin@feldrise.com>
2016-06-29 19:50:56 +0200

    Update FeldEngineErrors.cpp

--- src/FeldEngine/Core/FeldEngineErrors.cpp
+++ src/FeldEngine/Core/FeldEngineErrors.cpp
@@ -8,12 +8,12 @@
 namespace Fd 
 {
 	//function for print out a error message and exit the game
-	void fatalError(std::string errorString) {
+	void fatalError(std::string const& errorString) {
 		std::cout << errorString << std::endl;
 		std::cout << std::endl << "Press any key to exit...";
 		int k;
 		std::cin >> k;
 		SDL_Quit();
-		exit(69);
+		abort();
 	}
 }
