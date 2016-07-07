57089cbc45721022df7acaee81917f0e87e98028
S6066 <samy6066@yahoo.fr>
2016-07-07 11:10:46 +0200

    Update FeldEngineErrors.h

--- includes/FeldEngine/Core/FeldEngineErrors.h
+++ includes/FeldEngine/Core/FeldEngineErrors.h
@@ -5,9 +5,9 @@
 
 namespace Fd
 {
-	inline void log(const char* msg, ostream& os = std::cerr) //TODO: Add type of msg
+	inline void log(const std::string& msg, ostream& os = std::cerr) //TODO: Add type of msg
 	{
-		os << msg;
+		os << msg << std::flush;
 	}
 	extern void fatalError(std::string const& errorString);
 }
